76f3ddedd82f4befa5170ef150a19e804bf4f537
krasnosvar <45388508+krasnosvar@users.noreply.github.com>
2019-11-27 09:51:20 +0300

    Update centos6.sh

--- linux/os_centos/centos6.sh
+++ linux/os_centos/centos6.sh
@@ -3,3 +3,7 @@ export LANG="en_US.UTF-8"
 cat /etc/sysconfig/i18n
   LANG="en_US.UTF-8"
   SYSFONT="latarcyrheb-sun16"
+
+#узнать WWN на CentOS6
+scsi_id --replace-whitespace --whitelisted --device /dev/sdm
+for i in $(lsscsi -t| awk '{print $4}'); do echo "$i $(scsi_id --replace-whitespace --whitelisted --device $i)"; done
