a08d309ebaed5edcc59a5a23b3d4d6e65985b2b2
krasnosvar <45388508+krasnosvar@users.noreply.github.com>
2019-11-27 09:56:59 +0300

    Update centos6.sh

--- linux/os_centos/centos6.sh
+++ linux/os_centos/centos6.sh
@@ -4,6 +4,8 @@ cat /etc/sysconfig/i18n
   LANG="en_US.UTF-8"
   SYSFONT="latarcyrheb-sun16"
 
+#How do I find the FC ID (WWN) of a scsi device/LUN on Red Hat Enterprise Linux? 
+#https://access.redhat.com/articles/17054
 #узнать WWN на CentOS6
 scsi_id --replace-whitespace --whitelisted --device /dev/sdm
 for i in $(lsscsi -t| awk '{print $4}'); do echo "$i $(scsi_id --replace-whitespace --whitelisted --device $i)"; done
