e7a4b6e0426ea9fd42dc50e4c2e60640e2a7521e
krasnosvar <45388508+krasnosvar@users.noreply.github.com>
2019-12-04 12:48:13 +0300

    Update tee.sh

--- linux/bash_commands/tee.sh
+++ linux/bash_commands/tee.sh
@@ -7,3 +7,5 @@ command | tee file1.out file2.out file3.out
 command | tee -a file.out
 #записать в файл но не выводить на экран
 command | tee file.out >/dev/null
+#добавить deb репозиторий в /etc/apt/sources.list
+echo "deb http://packages.elastic.co/logstash/2.3/debian stable main" | sudo tee -a /etc/apt/sources.list
