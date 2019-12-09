ce25799818f901c6d9e79563d0b19a604888f8a9
krasnosvar <45388508+krasnosvar@users.noreply.github.com>
2019-12-09 15:40:43 +0300

    Update yum.sh

--- linux/bash_commands/yum.sh
+++ linux/bash_commands/yum.sh
@@ -9,3 +9,7 @@ https://habr.com/ru/post/301292/
 
 #delete all php but not "php-common"
 yum remove *php* -- -php-common 
+
+
+#How to know from which yum repository a package has been installed?
+repoquery -i rh-mongodb*
