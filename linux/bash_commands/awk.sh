e2da8a401e08831e3eaa5f177ca490f9f5de1119
krasnosvar <45388508+krasnosvar@users.noreply.github.com>
2019-12-20 08:09:27 +0300

    Update awk.sh

--- linux/bash_commands/awk.sh
+++ linux/bash_commands/awk.sh
@@ -6,3 +6,8 @@ ps aux | grep "dd "|awk '{print $2}'
 
 #kill all "dd" procecess
 for i in $(ps aux | grep "dd "|awk '{print $2}'); do kill -9 $i; done
+
+#Вывести кол-во ядер процессора(4 строка, 2 столбец)
+lscpu|awk 'NR == 4{print$2}'
+#умножить полученное значение на 2(например, для получения числа потоков)
+cores=$(lscpu|awk 'NR == 4{print$2}'); echo $(($cores*2))
