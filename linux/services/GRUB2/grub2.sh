c1540f63cf71a2bcc43dcf0d490717e85e73b767
krasnosvar <45388508+krasnosvar@users.noreply.github.com>
2019-12-20 14:25:07 +0300

    Update grub2.sh

--- linux/services/GRUB2/grub2.sh
+++ linux/services/GRUB2/grub2.sh
@@ -1,5 +1,5 @@
 #main file
 # If you change this file, run 'update-grub' afterwards to update
-# /boot/grub/grub.cfg.
+# /boot/grub2/grub.cfg.(never modify boot/grub2/grub.cfg!!!!!)
 /etc/default/grub
 
