1b23cb848ca77b37688024d3732a61a330fad618
krasnosvar <45388508+krasnosvar@users.noreply.github.com>
2019-12-20 14:36:10 +0300

    Update grub2.sh

--- linux/services/GRUB2/grub2.sh
+++ linux/services/GRUB2/grub2.sh
@@ -1,5 +1,8 @@
 #main file
+#in Ubuntu
 # If you change this file, run 'update-grub' afterwards to update
 # /boot/grub2/grub.cfg.(never modify boot/grub2/grub.cfg!!!!!)
+#in CentOS7
+#grub2-mkconfig > /boot/grub2/grub.cfg
 /etc/default/grub
 
