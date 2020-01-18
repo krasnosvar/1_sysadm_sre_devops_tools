2d61c2ac4d1a347f7f5815ddd8dee0d49e7aed9f
krasnosvar <krasnosvar@gmail.com>
2020-01-18 07:56:31 +0300

    script auto commit

--- linux/bash_commands/1users_and_groups.sh
+++ linux/bash_commands/1users_and_groups.sh
@@ -2,6 +2,9 @@
 #Изменение пароля одной строкой
 echo root:password | chpasswd
 cat file.txt| chpasswd
+echo "Password123" | passwd root --stdin > /dev/null #не везде сработает
+
+
 
 useradd
 adduser
