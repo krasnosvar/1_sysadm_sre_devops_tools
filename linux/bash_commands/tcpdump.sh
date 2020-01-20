56d5dd4961481e4f75370cc1af9f0242a9e54d89
krasnosvar <45388508+krasnosvar@users.noreply.github.com>
2020-01-20 15:48:21 +0300

    Update tcpdump.sh

--- linux/bash_commands/tcpdump.sh
+++ linux/bash_commands/tcpdump.sh
@@ -1,5 +1,8 @@
 #read pcap file
 tcpdump -ttttnnr 5701-01.pcap
 
+#снять трафик, идущий с интерфейса на ip-адрес
+tcpdump -i ens192 dst 10.8.62.44 -vv
+
 #снимать трафик с ens33, идущий на dst 8.8.8.8 исходящий с порта 53 
 tcpdump -i ens33 dst 8.8.8.8 and src port 53 -vv
