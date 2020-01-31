c206dd8062c0bf95255d22cc432aa1e69c5c58fe
krasnosvar <krasnosvar@gmail.com>
2020-01-31 15:05:53 +0300

    script auto commit

--- linux/bash_commands/ssh.sh
+++ linux/bash_commands/ssh.sh
@@ -15,3 +15,17 @@ Host userv
 HostName 192.168.43.100 
 User den
 Port 22
+
+#сгенерировать ключ одной командой(для скриптов), without passphrase
+# -b bits
+#              Specifies the number of bits in the key to create.  For RSA keys, the minimum size is 1024 bits and the default is 2048 bits.  Generally,
+#              2048 bits is considered sufficient.  DSA keys must be exactly 1024 bits as specified by FIPS 186-2.  For ECDSA keys, the -b flag deter‐
+#              mines the key length by selecting from one of three elliptic curve sizes: 256, 384 or 521 bits.  Attempting to use bit lengths other than
+#              these three values for ECDSA keys will fail.  Ed25519 keys have a fixed length and the -b flag will be ignored.
+# -t dsa | ecdsa | ed25519 | rsa
+#              Specifies the type of key to create.  The possible values are “dsa”, “ecdsa”, “ed25519”, or “rsa”.
+# -f filename
+#              Specifies the filename of the key file.
+# -N new_passphrase
+#              Provides the new passphrase.            
+ssh-keygen -b 2048 -t rsa -f /path/to/file/ssh_key_name -q -N ""
--- linux/services/letsencrypt-certbot/certbot.sh
+++ linux/services/letsencrypt-certbot/certbot.sh
@@ -2,5 +2,8 @@
 yum install letsencrypt -y
 
 #one-liner command to get certs(nginx must be on, certbot place file to check in "webroot-path")
+#centos
 certbot certonly --agree-tos --email blabla@mail.com --webroot --webroot-path /usr/share/nginx/html -n -d krasnosvar.devops.rebrain.srwx.net
+#ubuntu
+certbot certonly --agree-tos --email blabla@mail.com --webroot --webroot-path /var/www/html -n -d krasnosvar.devops.rebrain.srwx.net
 #certs will be in: /etc/letsencrypt/live/
