8e01ad299ea07a3eaf5e1f55726c53ebf70d8b9e
krasnosvar <45388508+krasnosvar@users.noreply.github.com>
2020-02-27 13:57:08 +0300

    Create udevadm.sh

---
+++ linux/bash_commands/udevadm.sh
@@ -0,0 +1,13 @@
+#While the udev program runs in the background on your Linux system, you can still inter-
+#act with it using the udevadm command-line tool. The udevadm command allows you to
+#send commands to the udev program. The format of the udevadm command is as follows:
+
+udevadm command [options]
+
+control #Modifies the internal state of udev
+info #Queries the udev database for device information
+monitor #Listens for kernel events and display them
+settle #Watches the udev event queue
+test #Simulates a udev event
+test-builtin #Runs a built-in device command for debugging
+trigger #Requests device events from the kernel
