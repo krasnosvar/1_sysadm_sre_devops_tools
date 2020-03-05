ad8624296f45d356d2e6d41799c7af80e74881bc
krasnosvar <krasnosvar@gmail.com>
2020-03-05 23:54:09 +0300

    script auto commit

--- linux/bash_commands/curl.sh
+++ linux/bash_commands/curl.sh
@@ -1,7 +1,22 @@
 #use curl without proxy
 curl  --noproxy "*"
+
+#Тип запроса определяется с ключем -X (GET идет по-умолчанию, можно не указывать):
+curl -X GET http://www.example.com
+curl -X POST http://www.example.com -d '{"variable": "value"}'
+curl -X POST http://www.example.com -d @filename.json
+
+
 #POST as JSON
 curl -H "Content-Type: application/json" \
   -X POST \
   --data '{"username":"xyz","password":"xyz"}' \
   http://localhost:3000/api/login
+
+
+
+curl --insecure \
+-H "Accept: application/json" \
+-H "Content-Type: application/json" \
+-H "Authorization: $AUTH" \
+--data @snapvra.json https://tcloud.corp.domain.ru/catalog-service/api/consumer/entitledCatalogItems/b7210569-ce4b-4110-b123-a58f8682fb67/requests| python -m json.tool
--- linux/services/SELinux/selinux.sh
+++ linux/services/SELinux/selinux.sh
@@ -1,10 +1,11 @@
+
+
+
+
 #через rsyslog на удаленный сервер отправлять введенные команды(то есть rsyslog должен логировать  ~/user/.bashrc)
 #с включенным SELinux
-
-Настройка rsyslog
-
+#Настройка rsyslog
 vi  /etc/rsyslog.d/bash.conf
-
 #################################################
 $ModLoad imfile
 $InputFileName /home/apilist/.bash_history
@@ -15,21 +16,15 @@ $InputFileFacility local7
 $InputRunFileMonitor
 ##
 $InputFilePollInterval 3
-
 local7.*                 @10.8.62.44:514
 ##################################################
-
 service rsyslog restart
-
 #настройка SELinux
 #поставить утилиты если нет
 yum install policycoreutils-python
-
 #проверить в какой группе файлы rsyslog( на эти же правила настроим bashrc)
 ls -Z /etc/rsyslog.d/
-
 #настройка bashrc
 chcon -t syslog_conf_t /home/local/.bash_history
-
 #проверка уходят ли пакеты на сервер
 tcpdump -i ens192 dst 10.8.62.44 -vv
