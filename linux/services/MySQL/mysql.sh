3fe34a7e4e3aa24c7e39c31ecb636c488d039249
krasnosvar <krasnosvar@gmail.com>
2020-03-11 01:30:37 +0300

    script auto commit

--- linux/services/MySQL/mysql.sh
+++ linux/services/MySQL/mysql.sh
@@ -1,10 +1,18 @@
 #https://mariadb.com/resources/blog/installing-mariadb-10-on-centos-7-rhel-7/
 #https://mariadb.com/resources/blog/how-to-install-mariadb-on-rhel8-centos8/
 #https://mariadb.com/kb/en/yum/
-
+#mariadb in centos
 curl -sS https://downloads.mariadb.com/MariaDB/mariadb_repo_setup | sudo bash
-
 yum install -y mariadb mariadb-server
+
+#main commands
+#show all databases
+SHOW DATABASES;
+#use database test
+use test;
+#show all in test
+SELECT * FROM test;
+
 
 #install db for wordpress
 $ mysql -u adminusername -p
