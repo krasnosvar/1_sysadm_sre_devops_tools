ce4a0e7e062b380bfdd92f685b17e5b174be3819
krasnosvar <45388508+krasnosvar@users.noreply.github.com>
2020-03-18 09:30:55 +0300

    Create edit_docx.py

---
+++ python/edit_docx.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+#скрипт позволяет редактировать шаблон docx, в данном примере значения переаются аргументами,но можно и по другому 
+#в самом docx редактируемое значение должно быть в двойных кавычках jinja2: {{ SCCN }}
+
+
+import sys
+import time
+from docxtpl import DocxTemplate
+#timestr = time.strftime("%Y%m%d-%H%M%S")
+doc = DocxTemplate("req.docx")
+path = sys.argv[4]
+context = { 'SCCN' : sys.argv[1], 'SCDEPART' : sys.argv[2], 'SCSAN' : sys.argv[3]}
+print((sys.argv[1::] ))
+print(context)
+doc.render(context)
+#doc.save(f"/usr/git/work/python/cert-parse/{timestr}.docx")
+doc.save(sys.argv[4])
