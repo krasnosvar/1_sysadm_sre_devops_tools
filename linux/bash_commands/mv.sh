a62fcc90040fca927dfeaff1d853267cc0089eee
krasnosvar <krasnosvar@gmail.com>
2020-03-30 11:34:29 +0300

    script auto commit

---
+++ linux/bash_commands/mv.sh
@@ -0,0 +1,9 @@
+-f ––force Overwrite any preexisting destination files with same name
+as DEST .
+-i ––interactive Ask before overwriting any preexisting destination files with the
+same name as DEST .
+-n ––no-clobber Do not overwrite any preexisting destination files with the same
+name as DEST .
+-u ––update Only overwrite preexisting destination files with the same name
+as DEST if the source file is newer.
+-v ––verbose Provide detaile
