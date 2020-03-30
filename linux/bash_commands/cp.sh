601c2f0c791fc2f267bb50aee5c1413fed2c23c2
krasnosvar <krasnosvar@gmail.com>
2020-03-30 11:34:29 +0300

    script auto commit

---
+++ linux/bash_commands/cp.sh
@@ -0,0 +1,14 @@
+-a ––archive recursive copy and keep all the files’ original
+attributes, permissions, ownership, and timestamps.
+-f ––force Overwrite any preexisting destination files with same name as DEST .
+-i ––interactive Ask before overwriting any preexisting destination files with
+same name as DEST .
+-n ––no-clobber Do not overwrite any preexisting destination files with same
+name as DEST .
+-R , -r ––recursive Copy a directory’s contents, and for any subdirectory within the
+original directory tree, consecutively copy its contents as well
+(recursive).
+-u ––update Only overwrite preexisting destination files with the same name
+as DEST , if the source file is newer.
+-v ––verbose Provide detailed command action information as command
+executes.
---
+++ linux/bash_commands/diff.sh
@@ -0,0 +1,39 @@
+#diff -y Dockerfile Dockerfile_test_diff 
+FROM alpine:latest                                              FROM alpine:latest
+LABEL authors="krasnosvar@gmail.com"                          | LABEL authors="krasnosvar@yandex.ru"
+
+RUN apk add --no-cache \                                        RUN apk add --no-cache \
+    openssh-client \                                                openssh-client \
+    sshpass \                                                       sshpass \
+    rsync \                                                         rsync \
+    python3 \                                                       python3 \
+    build-base \                                                    build-base \
+    python3-dev \                                                   python3-dev \
+    libffi-dev \                                                    libffi-dev \
+    openssl-dev \                                                   openssl-dev \
+    curl \                                                          curl \
+    tzdata \                                                        tzdata \
+&& pip3 install --upgrade --no-cache-dir pip \                  && pip3 install --upgrade --no-cache-dir pip \
+&& pip3 install --no-cache-dir ansible-lint \                   && pip3 install --no-cache-dir ansible-lint \
+&& apk del \                                                    && apk del \
+    python3-dev \                                                   python3-dev \
+    libffi-dev \                                                    libffi-dev \
+    build-base \                                                    build-base \
+    openssl-dev \                                                   openssl-dev \
+    gcc \                                                           gcc \
+    musl-dev \                                                      musl-dev \
+    linux-headers \                                                 linux-headers \
+    build-base \                                                    build-base \
+    g++ \                                                           g++ \
+&& rm -rf /var/cache/* && rm -rf /root/.cache/*                 && rm -rf /var/cache/* && rm -rf /root/.cache/*
+RUN if [ ! -e /usr/bin/pip ]; then ln -s /usr/bin/pip3 /usr/b   RUN if [ ! -e /usr/bin/pip ]; then ln -s /usr/bin/pip3 /usr/b
+if [[ ! -e /usr/bin/python ]]; then ln -sf /usr/bin/python3 /   if [[ ! -e /usr/bin/python ]]; then ln -sf /usr/bin/python3 /
+
+#diff -q Dockerfile Dockerfile_test_diff 
+Files Dockerfile and Dockerfile_test_diff differ
+
+#diff Dockerfile Dockerfile_test_diff 
+2c2
+< LABEL authors="krasnosvar@gmail.com"
+---
+> LABEL authors="krasnosvar@yandex.ru"
