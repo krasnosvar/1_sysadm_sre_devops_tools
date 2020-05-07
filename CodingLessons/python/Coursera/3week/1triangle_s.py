2b67abc056d1b5cf9dc2d171901daa5cd0543ca0
krasnosvar <krasnosvar@gmail.com>
2020-05-07 16:33:49 +0300

    script auto commit

--- CodingLessons/python/Coursera/3week/1triangle_s.py
+++ CodingLessons/python/Coursera/3week/1triangle_s.py
@@ -1,4 +1,4 @@
 a, b, c = int(input()), int(input()), int(input())
 p = (a + b + c) / 2
 S = ((p * (p - a) * (p - b) * (p - c)) ** 0.5)
-print(round(S, 6)) #округлить до 6 знаков после запятой
+print(round(S, 3)) #округлить до 3 знаков после запятой
