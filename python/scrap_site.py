143d4e5da0830bd0e2e91eb2edf3826047e6445d
krasnosvar <krasnosvar@gmail.com>
2020-05-21 07:31:03 +0300

    script auto commit

--- python/scrap_site.py
+++ python/scrap_site.py
@@ -1,12 +1,40 @@
-#https://docs.python-guide.org/scenarios/scrape/
-#sudo pip3 install lxml
-from lxml import html
-import requests
-#Next we will use requests.get to retrieve the web page with our data, parse it using the html module, and save the results in tree:
+#http://theautomatic.net/2017/09/19/robobrowser-automating-online-forms/
+#sudo pip3 install robobrowser
+#sudo pip3 install pandas
+#
+from robobrowser import RoboBrowser
+import pandas as pd
+from sys import argv
+testovoe_virazhen = argv[1]
 
-page = requests.get('http://localhost:8000')
-tree = html.fromstring(page.content)
-print((page.content))
+def interface_test_ex12(testovoe_virazhen):
+    '''Create RoboBrowser object
+       This will act similarly to a typical web browser'''
+    browser = RoboBrowser(parser='html.parser', history=True)
+    #browser = RoboBrowser(history=True)
+    
+    
+    browser.open('http://localhost:8000/')
+    forms = browser.get_forms()
+    form = forms[0]
+    #"operation" - name of form from "html" output
+    '''den@den-UX310UQK:qa-system$ curl -s localhost:8000/
+        <form action="/calc" method="post">
+            <input type="text" name="operation" />
+            <input type="submit" value="Посчитать" />
+        </form>
+    '''
+    form['operation'] = testovoe_virazhen
+    
+    
+    '''Submit form'''
+    browser.submit_form(form)
+    html = str(browser.parsed)
+    return html
 
-#fill forms
-http://theautomatic.net/2017/09/19/robobrowser-automating-online-forms/
+print(interface_test_ex12(testovoe_virazhen))
+# print(interface_test_ex12('2,*,3'))
+# print(interface_test_ex12('2,+,3'))
+# print(interface_test_ex12('3,-,3'))
+# print(interface_test_ex12('2,/,3'))
+# print(interface_test_ex12('2,**,3'))
