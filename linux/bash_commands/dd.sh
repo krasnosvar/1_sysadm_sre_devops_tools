9124765cbae6b50da062a02adc518e3a1658859a
krasnosvar <krasnosvar@gmail.com>
2020-05-23 20:02:25 +0300

    script auto commit

--- linux/bash_commands/dd.sh
+++ linux/bash_commands/dd.sh
@@ -1,2 +1,6 @@
 #erase external USB-stick
 sudo dd if=/dev/zero of=/dev/sdc bs=1M && sync
+
+#create boot usb
+#https://wiki.archlinux.org/index.php/USB_flash_installation_media
+dd bs=4M if=ubuntu-20.04-desktop-amd64.iso of=/dev/sdb status=progress oflag=sync
