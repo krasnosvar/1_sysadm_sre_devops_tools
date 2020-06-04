78570c5010b0720a0bab0c07119afd5b6685b9d5
krasnosvar <krasnosvar@gmail.com>
2020-06-05 00:01:19 +0300

    script auto commit

--- linux/bash_commands/2_oneliners.sh
+++ linux/bash_commands/2_oneliners.sh
@@ -38,3 +38,6 @@ cd /tmp/test
 find . -type f -exec sed -i  "s/OLDPASSWD/NEWPASSWD/g" {} +
 #Find in /etc/ and change IP-address in all files(*-asterisk if IP written with port(for example)), errors output in devnull
 find /etc/ -type f -exec sed -i 's/10.8.181.95*/10.5.10.149/g' {} + 2> /dev/null
+
+#clear deleted files
+for i in `find /proc/*/fd -ls | grep  '(deleted)' | awk {'print $11'}`; do echo  > $i && echo "Затер дисрипотр $i "; done
