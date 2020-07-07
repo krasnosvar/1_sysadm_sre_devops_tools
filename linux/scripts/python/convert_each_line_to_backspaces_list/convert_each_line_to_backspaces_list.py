cc8ceb07e225e718d6ce4b955ea9d2cf2fa833ea
krasnosvar <krasnosvar@gmail.com>
2020-07-07 14:38:01 +0300

    script auto commit

--- linux/scripts/python/convert_each_line_to_backspaces_list/convert_each_line_to_backspaces_list.py
+++ linux/scripts/python/convert_each_line_to_backspaces_list/convert_each_line_to_backspaces_list.py
@@ -1,6 +1,8 @@
 #https://stackoverflow.com/questions/3925614/how-do-you-read-a-file-into-a-list-in-python
 #https://stackoverflow.com/questions/42500944/how-to-change-the-list-separator-in-python
 with open('list.txt') as f:
+    #convert column to one-line with backspaces
     lines = f.read().replace('\n',' ')
+    #convert one-line with commas to column
     #lines = f.read().replace(', ','\n')
     print(lines)
