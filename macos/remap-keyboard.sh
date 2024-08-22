66c5b63570e74816bd6c5fca474afceebc4bbdc7
krasnosvar <krasnosvar@gmail.com>
2024-08-22 11:19:36 +0400

    add mac vscode lauout

--- macos/remap-keyboard.sh
+++ macos/remap-keyboard.sh
@@ -1,2 +1,7 @@
+# win-layout in system
 brew install --cask karabiner-elements
 https://github.com/rux616/karabiner-windows-mode
+
+
+# vscode, codium windows layout
+codium --install-extension smcpeak.default-keys-windows
