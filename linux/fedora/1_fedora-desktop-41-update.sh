0578bf8125223ffc0bc8020f83f477d896dc435e
krasnosvar <krasnosvar@gmail.com>
2025-02-11 22:44:13 +0400

    fix f41 script

--- linux/fedora/1_fedora-desktop-41-update.sh
+++ linux/fedora/1_fedora-desktop-41-update.sh
@@ -1,16 +1,20 @@
 # background colour #1d99f3
 
+# OS Utils, Repos
 #!/usr/bin/env bash
 sudo dnf upgrade --refresh
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 # system utils
-sudo dnf -y install dnf-plugins-core plasma-workspace-x11 sysfsutils
+#iostat, pidstat
+# https://github.com/sysstat/sysstat
+sudo dnf install -y git wget gnupg lsb-release apt-transport-https ca-certificates curl \
+  dnf-plugins-core plasma-workspace-x11 sysfsutils sysstat
 
 
 #Main OS apps
-sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-flatpak remote-add --user --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-sudo dnf install git wget gnupg lsb-release apt-transport-https ca-certificates curl \
-  flameshot audacity vlc telegram -y
+sudo dnf install flameshot audacity vlc telegram -y
+
 
 #Virtualization
 # https://docs.fedoraproject.org/en-US/quick-docs/virtualization-getting-started/
@@ -18,9 +22,7 @@ sudo dnf install @virtualization -y
 sudo systemctl start libvirtd
 sudo usermod -a -G libvirt den
 sudo usermod -a -G kvm den
-#iostat, pidstat
-# https://github.com/sysstat/sysstat
-sudo dnf install sysstat -y
+
 
 
 #Security
@@ -53,6 +55,7 @@ flatpak install --user flathub one.ablaze.floorp -y
 #SIP-protocol analyzer- sngrep
 #mtr combines the functionality of the traceroute and ping programs in a single network diagnostic tool.
 #mtr -i 0.1 yoursite.com
+# https://httpie.io/docs/cli/fedora
 sudo dnf install arp-scan mtr wireshark traceroute openssh-server arping httpie \
   sshuttle openconnect NetworkManager-openconnect openfortivpn -y
 sudo usermod -a -G wireshark den
@@ -63,63 +66,6 @@ sudo usermod -a -G wireshark den
 # https://developer.hashicorp.com/terraform/tutorials/aws-get-started/install-cli
 sudo dnf config-manager addrepo --from-repofile=https://rpm.releases.hashicorp.com/fedora/hashicorp.repo
 sudo dnf -y install terraform
-# https://httpie.io/docs/cli/fedora
-sudo dnf install httpie -y
-#docker
-# https://docs.docker.com/engine/install/fedora/#install-using-the-repository
-sudo dnf config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-sudo usermod -a -G docker den
-#kubectl
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
-sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
-# helm
-# age
-# https://github.com/FiloSottile/age#installation
-sudo dnf install helm age httpie yq jq tmux byobu -y
-flatpak install flathub io.dbeaver.DBeaverCommunity -y
-# sops
-# https://gist.github.com/patrickmslatteryvt/d531c5ae4598fd4c9d508833bde6c7c0
-SOPS_VERSION=$(curl -s https://api.github.com/repos/getsops/sops/releases/latest | jq .tag_name | tr -d '"')
-dnf install -y https://github.com/getsops/sops/releases/download/${SOPS_VERSION}/sops-${SOPS_VERSION:1}-1.x86_64.rpm
-sops --version
-# helm plugins
-helm plugin install https://github.com/jkroepke/helm-secrets --version v4.4.2
-helm plugin install https://github.com/databus23/helm-diff
-# psql
-sudo dnf install postgresql -y
-# dbeaver
-# https://dbeaver.io/download/
-sudo dnf install https://dbeaver.io/files/dbeaver-ce-latest-stable.x86_64.rpm -y
-
-#programming, development
-sudo dnf install python3 python3.9 python3.10 python3.12 -y
-#install go
-# https://developer.fedoraproject.org/tech/languages/go/go-installation.html
-sudo dnf install golang -y
-#for java keytool
-dnf search openjdk
-# git
-sudo dnf install vim neovim -y
-git config --global user.name "krasnosvar"
-git config --global user.email "krasnosvar@gmail.com"
-git config --global color.ui auto
-git config --global core.editor "nvim"
-# install VScodium - free vscode
-sudo dnf install codium -y
-# https://vscodium.com/#install
-sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
-printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
-# Migrating from VS Code to VS Codium on Linux
-# https://www.roboleary.net/tools/2022/06/13/migrate-from-vscode-to-vscodium-on-linux.html
-# copy everything
-# cp -r $HOME/.config/Code/User/* $HOME/.config/VSCodium/User
-# copy extensions
-# sudo cp -R ~/.vscode/extensions ~/.vscode-oss
-mkdir -p $HOME/.config/VSCodium/User/
-cp files/vscode/settings.json $HOME/.config/VSCodium/User/settings.json
-mkdir -p $HOME/.config/Code/User/
-cp files/vscode/settings.json $HOME/.config/Code/User/settings.json
 # install tofu - free terraform
 # https://opentofu.org/docs/intro/install/rpm/
 # Download the installer script:
@@ -131,11 +77,72 @@ chmod +x install-opentofu.sh
 ./install-opentofu.sh --install-method rpm
 # Remove the installer:
 rm install-opentofu.sh
+#docker
+# https://docs.docker.com/engine/install/fedora/#install-using-the-repository
+sudo dnf config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+sudo usermod -a -G docker den
+#kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/arm64/kubectl"
+sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+# helm
+# age
+# https://github.com/FiloSottile/age#installation
+sudo dnf install helm age httpie yq jq tmux byobu awscli2 -y
+# sops
+# https://gist.github.com/patrickmslatteryvt/d531c5ae4598fd4c9d508833bde6c7c0
+SOPS_VERSION=$(curl -s https://api.github.com/repos/getsops/sops/releases/latest | jq .tag_name | tr -d '"')
+dnf install -y https://github.com/getsops/sops/releases/download/${SOPS_VERSION}/sops-${SOPS_VERSION:1}-1.x86_64.rpm
+sops --version
+# helm plugins
+helm plugin install https://github.com/jkroepke/helm-secrets --version v4.6.2
+helm plugin install https://github.com/databus23/helm-diff
+# psql
+sudo dnf install postgresql -y
+# dbeaver
+# https://dbeaver.io/download/
+sudo dnf install https://dbeaver.io/files/dbeaver-ce-latest-stable.x86_64.rpm -y
+
+
+#programming, development
+sudo dnf install python3 python3.9 python3.10 python3.12 -y
+#install go
+# https://developer.fedoraproject.org/tech/languages/go/go-installation.html
+sudo dnf install golang -y
+#for java keytool
+dnf search openjdk
+# git, editors - nvim, vscode
+sudo dnf install vim neovim -y
+sudo dnf copr enable vitallium/neovim-default-editor
+sudo dnf install neovim-default-editor --allowerasing
+git config --global user.name "krasnosvar"
+git config --global user.email "krasnosvar@gmail.com"
+git config --global color.ui auto
+git config --global core.editor "nvim"
+# vscode
+# https://code.visualstudio.com/docs/setup/linux
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+# install VScodium - free vscode
+# https://vscodium.com/#install
+sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h" | sudo tee -a /etc/yum.repos.d/vscodium.repo
+sudo dnf check-update
+sudo dnf install code codium -y
+# Migrating from VS Code to VS Codium on Linux
+# https://www.roboleary.net/tools/2022/06/13/migrate-from-vscode-to-vscodium-on-linux.html
+# copy everything
+# cp -r $HOME/.config/Code/User/* $HOME/.config/VSCodium/User
+# copy extensions
+# sudo cp -R ~/.vscode/extensions ~/.vscode-oss
+mkdir -p $HOME/.config/VSCodium/User/
+cp files/vscode/settings.json $HOME/.config/VSCodium/User/settings.json
+mkdir -p $HOME/.config/Code/User/
+cp files/vscode/settings.json $HOME/.config/Code/User/settings.json
 
 
 #VScode extensions
 #https://stackoverflow.com/questions/34286515/how-to-install-visual-studio-code-extensions-from-command-line
-
 # sudo -u den codium --install-extension golang.go
 # sudo -u den codium --install-extension ms-toolsai.jupyter
 sudo -u den codium --install-extension ms-python.python
