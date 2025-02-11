757eb8ddc84b2dda79a555c85badfb81db4e0cbc
krasnosvar <krasnosvar@gmail.com>
2025-02-11 22:44:13 +0400

    fix f41 script

--- linux/1_shell_bash_commands/1_kernel_hard_system_ps/systemd_sysV/loginctl.sh
+++ linux/1_shell_bash_commands/1_kernel_hard_system_ps/systemd_sysV/loginctl.sh
@@ -11,3 +11,11 @@ loginctl terminate-session
 #Вывести информацию о состоянии текущего сеанса (или любого другого, если добавить id), 
 #включая список дочерних процессов и номер виртуальной консоли:
 loginctl session-status
+
+
+# check wayland or x11
+# https://unix.stackexchange.com/questions/202891/how-to-know-whether-wayland-or-x11-is-being-used
+loginctl
+loginctl show-session <SESSION_ID> -p Type
+# or in one command
+loginctl show-session $(awk '/tty/ {print $1}' <(loginctl)) -p Type | awk -F= '{print $2}'
